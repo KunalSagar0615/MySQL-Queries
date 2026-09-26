fb9c7f4ff1350c9bd29f88252f2f776c8d1f7aa4
Kunal Ananda Sagar <kunalsagar0615@gmail.com>
2026-09-26 17:40:48 +0530

    Find the highest-paid employee in each salary grade.

---
+++ Q163.sql
@@ -0,0 +1,1 @@
+ SELECT e.ename, s.grade, e.sal FROM emp e JOIN salgrade s ON e.sal BETWEEN s.losal AND s.hisal WHERE e.sal = (SELECT MAX(e2.sal) FROM emp e2 JOIN salgrade s2 ON e2.sal BETWEEN s2.losal AND s2.hisal WHERE s2.grade = s.grade);
